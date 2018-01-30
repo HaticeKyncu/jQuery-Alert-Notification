5d8bcaa5c92c6b03bb99a7a362ee8621069f8d94
Hatice Koyuncu <ddc1132@DDCDOMAIN.LOCAL>
2018-01-30 15:57:57 +0300

    Message notification

---
+++ JQueryProject/Login.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JQueryProject
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        //[System.Web.Services.WebMethod]
+        //public static int jQueryPageMethod(string user, string password)
+        //{
+        //    OracleConnect DbConn = new OracleConnect();
+        //    int kayit_sayisi = 0;
+        //    string sql = "select count(FRMNUM) from URTTNM.USRTNM " + " WHERE URTTNM.USRTNM.USERNAME='" + user + "' AND URTTNM.USRTNM.SIFRE='" + password + "'";
+        //    DbConn.Sayac(sql);
+        //    kayit_sayisi = Convert.ToInt32(DbConn.GeriDonenRakam);
+        //    if (kayit_sayisi > 0)
+        //    {
+        //        string sorgu = "SELECT * FROM URTTNM.USRTNM" + " WHERE URTTNM.USRTNM.USERNAME='" + user + "' AND URTTNM.USRTNM.SIFRE='" + password + "'";
+        //        DbConn.RaporWhile(sorgu);
+        //        while (DbConn.MyDataReader.Read())
+        //        {
+        //            HttpContext.Current.Session["kullanici"] = user;
+        //            string yetki = DbConn.MyDataReader.GetValue(26).ToString();
+        //            if (yetki == "E")
+        //            {
+        //                return 1;
+        //            }
+        //        }
+        //        DbConn.Close();
+        //        return 0;
+        //    }
+        //}
+        //}
+    }
+}
